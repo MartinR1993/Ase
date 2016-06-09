4b03c9f4acdba075d937a58db0565358cb1df2fe
IamHexor <ykhexor@hotmail.com>
2016-06-09 14:05:56 +0200

    HEJ

--- Ase/src/connector/connector.java
+++ Ase/src/connector/connector.java
@@ -4,31 +4,40 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
-import java.util.Scanner;
 
 
 public class connector {
 	public static void main(String[] args) {
-		
-	
+
+
+
 		try {
+			boolean run = true;
 			Socket socket = new Socket("localhost", 8000);
 			DataOutputStream os = new DataOutputStream(socket.getOutputStream());
 			DataInputStream is = new DataInputStream(socket.getInputStream());
-
 			BufferedReader scan = new BufferedReader (new InputStreamReader(System.in));
-			
-//			os.writeBytes(scan.readLine()+ "\r\n");
-			os.writeBytes("B 200\r\n");
-			os.writeBytes("Q\r\n");
 
-			
+			while (run) {
+
+				String command = scan.readLine().toString();
+
+				os.writeBytes(command+ "\r\n");
+
+				if (command.equals("Q")) {
+					run = false;
+					scan.close();
+				}
+			}
+
+
+			is.close();
+			os.close();
+			socket.close();
+
 		} catch (Exception e) {
 			System.err.println("Exception:  " + e);
-	} 
+		} 
 	}
 }
