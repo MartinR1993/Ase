fcbe592674b2b645db1e8c84ff41ce3a6414f224
IamHexor <ykhexor@hotmail.com>
2016-06-10 08:54:22 +0200

    LÆKKARD

--- Ase/src/connector/ConnectorVaegt.java
+++ Ase/src/connector/ConnectorVaegt.java
@@ -3,41 +3,87 @@ package connector;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class ConnectorVaegt {
+
+	static Socket socket;
+	
 	public static void main(String[] args) {
 
-
-
 		try {
-			boolean run = true;
-			Socket socket = new Socket("localhost", 8000);
-			DataOutputStream os = new DataOutputStream(socket.getOutputStream());
-			DataInputStream is = new DataInputStream(socket.getInputStream());
-			BufferedReader scan = new BufferedReader (new InputStreamReader(System.in));
+			socket = new Socket("169.254.2.2", 8000);
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		Thread send = new Thread(new Runnable() {
 
-			while (run) {
+			@Override
+			public void run() {
 
-				String command = scan.readLine().toString();
+				try {
+					boolean run = true;
+					
+					DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+//					DataInputStream is = new DataInputStream(socket.getInputStream());
+					BufferedReader scan = new BufferedReader (new InputStreamReader(System.in));
 
-				os.writeBytes(command+ "\r\n");
+					while (run) {
 
-				if (command.equals("Q")) {
-					run = false;
-					scan.close();
+						String command = scan.readLine().toString();
+
+						os.writeBytes(command+ "\r\n");
+
+						if (command.equals("Q")) {
+							run = false;
+							scan.close();
+						}
+					}
+
+				} catch (Exception e) {
+					System.err.println("Exception:  " + e);
 				}
+
 			}
+		});
 
+		Thread receive = new Thread(new Runnable() {
 
-			is.close();
-			os.close();
-			socket.close();
+			@Override
+			public void run() {
 
-		} catch (Exception e) {
-			System.err.println("Exception:  " + e);
-		} 
+				DataInputStream is;
+				try {
+					is = new DataInputStream(socket.getInputStream());
+				while(true){
+					String read = is.readLine();
+					System.out.println(read);
+				}
+				
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				
+				
+			}
+		});
+
+		send.start();
+		receive.start();
+
 	}
 }
